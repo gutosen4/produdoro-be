3dbdcd074962d78ecb519981ff3c6a81bcf9c7da
Guto Sena <augusto_sena@hotmail.com>
2024-01-02 13:08:08 -0300

    mapeia agregado de tarefa

---
+++ src/main/java/dev/wakandaacademy/produdoro/tarefa/domain/Tarefa.java
@@ -0,0 +1,35 @@
+package dev.wakandaacademy.produdoro.tarefa.domain;
+
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import dev.wakandaacademy.produdoro.usuario.domain.StatusUsuario;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@Document(collection = "Tarefa")
+public class Tarefa {
+	@Id
+	private UUID IdTarefa;	
+	private String nome;
+	private String descricao;
+	@Indexed
+	private UUID IdUsuario;
+	@Indexed	
+	private UUID IdArea;
+	@Indexed
+	private UUID IdProjeto;	
+	@Builder.Default
+	private StatusTarefa status = StatusTarefa.A_FAZER;
+
+}
