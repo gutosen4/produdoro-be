5b3daaca738c9466a1dc05e1e79fa63c248f768c
Guto Sena <augusto_sena@hotmail.com>
2024-01-07 14:41:36 -0300

    Ultimas atualizaçÃões da aula anterior - swagger instável

---
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/domain/UsuarioAPI.java
@@ -0,0 +1,17 @@
+package dev.wakandaacademy.produdoro.usuario.domain;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping(value = "/v1/usuario")
+public interface UsuarioAPI {
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    UsuarioCriadoResponse postNovoUsuario(@Valid UsuarioNovoRequest usuarioNovo);
+}
---
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/domain/UsuarioController.java
@@ -0,0 +1,17 @@
+package dev.wakandaacademy.produdoro.usuario.domain;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@Log4j2
+public class UsuarioController implements UsuarioAPI {
+
+    @Override
+    public UsuarioCriadoResponse postNovoUsuario(@Valid UsuarioNovoRequest usuarioNovo) {
+        log.info("[start] UsuarioController - postNovoUsuario");
+        return null;
+    }
+}
