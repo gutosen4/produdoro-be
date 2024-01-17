37b24053b3e2806105b07af39686caaddfae58c6
Guto Sena <augusto_sena@hotmail.com>
2024-01-16 22:39:51 -0300

    PrAinda precisando de correção na validação dos emails e senhas

--- src/main/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioRepository.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/application/service/UsuarioRepository.java
@@ -3,6 +3,5 @@ package dev.wakandaacademy.produdoro.usuario.application.service;
 import dev.wakandaacademy.produdoro.usuario.domain.Usuario;
 
 public interface UsuarioRepository {
-
     Usuario salva(Usuario usuario);
 }
--- src/main/java/dev/wakandaacademy/produdoro/usuario/domain/Usuario.java
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/domain/Usuario.java
@@ -23,9 +23,9 @@ import lombok.NoArgsConstructor;
 @Document(collection = "Usuario")
 public class Usuario {
 	@Id
-	private UUID IdUsuario;
+	private UUID idUsuario;
 	@Email
-	@Indexed
+	@Indexed(unique = true)
 	private String email;
 	private ConfiguracaoUsuario configuracao;
 	@Builder.Default
@@ -34,6 +34,7 @@ public class Usuario {
 	private Integer quantidadePomodorosPausaCurta = 0;
 
 	public Usuario(UsuarioNovoRequest usuarioNovo, ConfiguracaoPadrao configuracaoPadrao) {
+		this.idUsuario = UUID.randomUUID();
 		this.email = usuarioNovo.getEmail();
 		this.status = StatusUsuario.FOCO;
 		this.configuracao = new ConfiguracaoUsuario(configuracaoPadrao);
